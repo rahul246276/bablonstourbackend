808d0e9f81df3ad89dbe22b0c3dcc77dab45964b
developer786-cloud <developerbablons786@gmail.com>
2026-07-27 16:03:35 +0530

    fixed hotel routes error

--- routes/adminHotelRoutes.js
+++ routes/adminHotelRoutes.js
@@ -14,9 +14,9 @@ const {
 const router = express.Router()
 
 router.use(protect)
-router.get('/', authorize('super_admin', 'admin'), validate(listHotelsSchema), hotelController.listHotels)
+router.get(/\/?$/, authorize('super_admin', 'admin'), validate(listHotelsSchema), hotelController.listHotels)
 router.get('/:id', authorize('super_admin', 'admin'), validate(hotelIdSchema), hotelController.getHotelById)
-router.post('/', authorize('super_admin'), validate(createHotelSchema), hotelController.createHotel)
+router.post(/\/?$/, authorize('super_admin'), validate(createHotelSchema), hotelController.createHotel)
 router.patch('/:id', authorize('super_admin'), validate(updateHotelSchema), hotelController.updateHotel)
 router.delete('/:id', authorize('super_admin'), validate(hotelIdSchema), hotelController.deleteHotel)
 router.patch('/:id/status', authorize('super_admin'), validate(toggleHotelStatusSchema), hotelController.toggleHotelStatus)
